fc20b950f0039863fe09354e9ff7c94810f1d0f0
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-09-02 01:18:32 -0500

    Create Heavy.java

---
+++ src/interfaz/Heavy.java
@@ -0,0 +1,62 @@
+package mundo;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+/**
+ * @author josuerom
+ */
+public class Heavy {
+    public static void main(String[] args) {
+        pc2Read();
+    }
+
+    public static void convert(String[] arr) {
+        int sumaA = 0, sumaB = 0;
+        String a[], b[];
+
+        a = arr[0].split("");
+        b = arr[1].split("");
+
+        for (int i = 0; i < a.length; i++) {
+            sumaA += Integer.valueOf(a[i]);
+            if (i == 0) {
+                for (int j = 0; j < b.length; j++) {
+                    sumaB += Integer.valueOf(b[j]);
+                }
+            }
+        }
+        process(sumaA, sumaB, a.length, b.length);
+    }
+    public static void process(int sumaA, int sumaB, int digitsA, int digitsB) {
+        int weightA = digitsA * sumaA;
+        int weightB = digitsB * sumaB;
+
+        if (weightA > weightB) {
+            System.out.println("1");
+        } else if (weightA < weightB) {
+            System.out.println("2");
+        } else if (weightA == weightB) {
+            System.out.println("0");
+        }
+    }
+
+    public static void pc2Read() {
+        String arr[], aux[], line = "";
+        int addA = 0, addB = 0;
+        BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
+
+        try {
+            line = buffer.readLine();
+            arr = line.split(" ");
+
+            buffer.close();
+            convert(arr);
+        } catch (NullPointerException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
