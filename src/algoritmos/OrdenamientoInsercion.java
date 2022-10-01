332e681e2356124b501398e83ca3c4018e2fabb2
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-10-01 17:24:09 -0500

    Update OrdenamientoInsercion.java

--- src/algoritmos/OrdenamientoInsercion.java
+++ src/algoritmos/OrdenamientoInsercion.java
@@ -1,23 +1,28 @@
 package algoritmos;
 
+import java.util.Arrays;
+
 public class OrdenamientoInsercion {
 
    public static void main(String[] args) {
-      int[] arr = {1, 10, 8, 7, 6, 5, 4, 3, 2, 9};
-      int max = 0;
+      int[] arr = {5, 1, 8, 7, 6, 10, 4, 3, 2, 9}; // 1
 
-      for (int i = 0; i < arr.length-1; i++) {
-         for (int j = 1; j < arr.length; j++) {
-            if (arr[j-1] > arr[j]) {
-               max = arr[j-1];
-               arr[j-1] = arr[j];
-               arr[j] = max;
-            }
+      for (int i = 1; i < arr.length; i++) {       // n
+         int actual = arr[i];                      // n
+         int j = i-1;                              // n
+
+         while (j >= 0 && arr[j] > actual) {       // n^2
+            arr[j+1] = arr[j];                     // n^2
+            j--;                                   // n^2
          }
+         arr[j+1] = actual;                        // n
       }
-
-      for (int e : arr) {
-         System.out.print(e + " ");
-      }
+      
+      System.out.println(Arrays.toString(arr));    // 1
+      /*
+         (Límite superior) Notación Big O. Representa el comportamiento en el peor de los casos 
+         La complejidad del algoritmo InsertionShort es de O(n^2) debido a que si simplificamos
+         --> 3n^2 + 4n + 2 = O(n^2)
+      */
    }
 }
