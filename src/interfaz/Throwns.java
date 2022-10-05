3292f84d1872185f6ec8077693a99535de4f12fd
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-10-05 13:44:29 -0500

    Update Throwns.java

--- src/interfaz/Throwns.java
+++ src/interfaz/Throwns.java
@@ -1,6 +1,7 @@
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.util.Arrays;
 
 public class Throwns {
     
@@ -9,24 +10,27 @@ public class Throwns {
     }
     
     public static void process(byte n[], String vec[]) {
-        byte n = n[0]; //número de estudiantes
-        byte k = n[1]; //cantidad de comandos que lanza Daenerys
+        byte students = n[0]; //número de estudiantes
+        byte commands = n[1]; //cantidad de comandos que lanza Daenerys
+        System.out.println(Arrays.deepToString(vec));
     }
     
     public static void pc2Read() {
-        String vec[], line;
-        byte n[] = new byte[2];
+        String vec[], n[], line;
+        byte student[] = new byte[2];
   
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
         
         try {
             line = buffer.readLine();
-            n = (byte) line.split(" ");
+            n = line.split(" ");
+            student[0] = Byte.valueOf(n[0]);
+            student[1] = Byte.valueOf(n[1]);
             
             line = buffer.readLine();
             vec = line.split(" ");
             buffer.close();
-            process(n, vec);
+            process(student, vec);
         } catch (NullPointerException e) {
             e.printStackTrace();
         } catch (IOException e) {
