f7f2b85652c01038ef79f5cbaecc77c2015b25d2
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-10-05 21:09:45 -0500

    Update Drinks.java

--- src/interfaz/Drinks.java
+++ src/interfaz/Drinks.java
@@ -1,9 +1,11 @@
 package interfaz;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
 public class Drinks {
+
     public static void main(String[] args) {
         pc2Read();
     }
@@ -15,7 +17,7 @@ public class Drinks {
             double p = Double.parseDouble(s);
             result += (p != 0) ? (p / n) : 0;
         }
-        System.out.printf("%.12f", result);
+        System.out.printf("%,.12f", result);
     }
 
     public static void pc2Read() {
@@ -29,9 +31,9 @@ public class Drinks {
             n = Integer.parseInt(line);
 
             line = buffer.readLine();
+            buffer.close();
             vec = line.split(" ");
             process(n, vec);
-            buffer.close();
         } catch (NullPointerException e) {
             e.printStackTrace();
         } catch (IOException e) {
