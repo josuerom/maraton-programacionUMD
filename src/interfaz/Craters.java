9f8cd17a3dbf537e7f12bd13bf00ba794f4a8a5e
Josué Romero <79048209+josuerom@users.noreply.github.com>
2022-10-05 20:56:49 -0500

    Update Craters.java

--- src/interfaz/Craters.java
+++ src/interfaz/Craters.java
@@ -4,39 +4,37 @@ import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-/**
- * @author josuerom
- */
 public class Craters {
+    private static final double PI = 3.14;
     private static double result;
     private static double desviacion;
-    
+
     public static void main(String[] args) {
         pc2Read();
     }
-    
+
     public static void process(double r) {
-        result += (2 * 3.14) * r;
+        result += (2 * PI) * r;
         desviacion += 35.096;
     }
-    
+
     public static void pc2Read() {
         String vec[], line = "";
         int n;
-        
+
         BufferedReader buffer = new BufferedReader(new InputStreamReader(System.in));
-        
+
         try {
             line = buffer.readLine();
             n = Integer.parseInt(line);
-            
+
             while (n-- >= 1) {
                 line = buffer.readLine();
                 vec = line.split(" ");
                 double radio = Double.parseDouble(vec[2]);
                 process(radio);
             }
-            
+
             System.out.printf("%.3f\n", (result - desviacion));
             buffer.close();
         } catch (IOException e) {
