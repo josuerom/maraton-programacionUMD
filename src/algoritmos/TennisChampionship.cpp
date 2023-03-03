dce568a5472a99b3c79b2f7100b8d97d9153b1da
Josué Romero <79048209+josuerom@users.noreply.github.com>
2023-03-03 17:22:33 -0500

    Actividad No. 3 DA

---
+++ src/algoritmos/TennisChampionship.cpp
@@ -0,0 +1,13 @@
+/**
+ * author:     josuerom
+ * created:    03/03/23 17:19
+ * complexity: O(p)
+**/
+#include <iostream>
+#include <stdio.h>
+
+int main() {
+  unsigned long long int p;
+  while (scanf("%llu", &p)==1) printf("%llu\n", (p-1));
+  return 0;
+}
