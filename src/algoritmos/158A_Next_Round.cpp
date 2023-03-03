acc421b7dc5b3ac0a74188dfa4256911de44f0ec
Josué Romero <79048209+josuerom@users.noreply.github.com>
2023-03-03 17:26:08 -0500

    Actividad No. 1 DA

--- src/algoritmos/158A_Next_Round.cpp
+++ src/algoritmos/158A_Next_Round.cpp
@@ -1,18 +1,18 @@
-/******************************************
-* @AUTHOR:            JOSUEROM            *
-* @UNIVERSITY: MINUTE OF GOD OF COLOMBIA  *
-******************************************/
+/**
+* author:  josuerom
+* created: 13/02/23 14:57
+**/
 #pragma GCC optimize("03,unroll-loops")
 #include <iostream>
 using namespace std;
 
 int main() {
-   ios::sync_with_stdio(false); cin.tie(0); cout.tie(0);
-   short n,k, amount=0;
+   ios::sync_with_stdio(false); cin.tie(0);
+   short n, k, amount=0;
    cin >> n >> k;
    short vec[n];
    for (short i = 0; i < n; i++) cin >> vec[i];
    for (short j = 0; j < n; j++) if (vec[j] >= vec[k-1] && vec[j] > 0) amount++;
-   cout << amount;
+   cout << amount << '\n';
    return 0;
 }
